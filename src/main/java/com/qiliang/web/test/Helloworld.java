8efd53480e08e4190c6a911406021aa4a73aa773
sunhan1231 <2991802811@qq.com>
2020-08-04 10:26:59 +0800

    first test

--- src/main/java/com/qiliang/web/test/Helloworld.java
+++ src/main/java/com/qiliang/web/test/Helloworld.java
@@ -2,7 +2,7 @@ package com.qiliang.web.test;
 
 public class Helloworld {
     public static void main(String args[]){
-        System.out.println("hasaasshhaha");
+        System.out.println("hasaassfhhaha");
     }
 
 }
